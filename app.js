d7e5784434fd3f562925c32239698dcbb2f935a8
Sakshi2k <sakshi2k@yahoo.co.in>
2020-04-28 00:02:24 +0530

    temp-change

--- app.js
+++ app.js
@@ -12,52 +12,52 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-    const firstName = req.body.fname;
-    const lastName = req.body.lname;
-    const phNum = req.body.phNum;
-    const email = req.body.email;
+    // const firstName = req.body.fname;
+    // const lastName = req.body.lname;
+    // const phNum = req.body.phNum;
+    // const email = req.body.email;
 
-    // console.log(firstName, lastName, email);
-    const data = {
-    members : [
-        {
-        email_address: email,
-        status : "subscribed",
-        merge_fields : {
-            FNAME : firstName,
-            LNAME : lastName,
-            PHONE : phNum
-        }
-        }
-    ]   
-    };
+    // // console.log(firstName, lastName, email);
+    // const data = {
+    // members : [
+    //     {
+    //     email_address: email,
+    //     status : "subscribed",
+    //     merge_fields : {
+    //         FNAME : firstName,
+    //         LNAME : lastName,
+    //         PHONE : phNum
+    //     }
+    //     }
+    // ]   
+    // };
 
-    const jsonData = JSON.stringify(data);
+    // const jsonData = JSON.stringify(data);
 
-    const url = "https://us8.api.mailchimp.com/3.0/lists/529cffc56a";
-    const options = {
-        method: "POST",
-        auth: "saku:2ed996a859eb298e8a6e18646ed8e9b1-us8"
-    };
+    // const url = "https://us8.api.mailchimp.com/3.0/lists/529cffc56a";
+    // const options = {
+    //     method: "POST",
+    //     auth: "saku:2ed996a859eb298e8a6e18646ed8e9b1-us8"
+    // };
 
-    const request = https.request(url, options, function(response) {
-        response.on("data", (data) => {
-            console.log(JSON.parse(data));
-        })
+    // const request = https.request(url, options, function(response) {
+    //     response.on("data", (data) => {
+    //         console.log(JSON.parse(data));
+    //     })
 
 
-        if(response.statusCode ===200){
-            res.sendFile(__dirname + "/success.html");
-        }
-        else res.sendFile(__dirname + "/failure.html");
-
+    //     if(response.statusCode ===200){
+    //         res.sendFile(__dirname + "/success.html");
+    //     }
+    //     else res.sendFile(__dirname + "/failure.html");
+    res.send("<h1>Hey</h1>");
     })
 
 
-    request.write(jsonData);
-    request.end();
+//     request.write(jsonData);
+//     request.end();
 
-});
+// });
 
 app.post("/failure",(req, res) => {
     res.redirect("/");
@@ -73,3 +73,5 @@ app.listen(process.env.PORT || 3000, () =>{
 
 //List ID:
 // 529cffc56a
+
+//https://infinite-wildwood-17047.herokuapp.com/
